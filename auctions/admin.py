a9926f15f54e834701f989e3fbf8d8a649d3d24a
Alekky09 <54728966+Alekky09@users.noreply.github.com>
2020-09-06 17:59:38 +0200

    Initial commit

--- auctions/admin.py
+++ auctions/admin.py
@@ -5,6 +5,7 @@ from .models import User, Listing, Bid, Comment, Category
 class CommentAdmin(admin.ModelAdmin):
     readonly_fields = ('date',)
 
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Listing)
